ebe91b621968fc117dd20f1c0eb64313cdc738c2
Victor Andres Marimon Mendoza <vmarimon1@udi.edu.co>
2024-05-11 16:53:02 -0500

    feat se agregan funciones

--- servicios/consultas_servicios.py
+++ servicios/consultas_servicios.py
@@ -7,12 +7,23 @@ from datosGenerales.datos import *
 RUTA_JSON = RUTA_JSON = "C:/Users/PC/Desktop/Proyecto---Campus/servicios/servicios.json"
 
 
-def referencia_servicios():
+def referencia_servicios(categoria):
     datos = cargar_datos(RUTA_JSON)
 
     referencias = []
 
-    for i in datos["servicios"][0]["internet"]:
+    for i in datos["servicios"][0][categoria]:
         referencias.append(str(i["referencia"]))
+    
+    return referencias
+
+def tipo_servicios():
+    datos = cargar_datos(RUTA_JSON)
+
+    referencias = []
+
+    for i in datos["servicios"][0]:
+
+        referencias.append(str(i))
     
     return referencias
