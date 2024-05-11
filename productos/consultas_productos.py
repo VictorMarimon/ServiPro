190c120da817a173a6203d12a13ec6aff1c31b3c
Victor Andres Marimon Mendoza <vmarimon1@udi.edu.co>
2024-05-11 16:52:51 -0500

    refactor arreglos funciones

--- productos/consultas_productos.py
+++ productos/consultas_productos.py
@@ -6,12 +6,23 @@ from datosGenerales.datos import *
 
 RUTA_JSON = "C:/Users/PC/Desktop/Proyecto---Campus/productos/productos.json"
 
-def referencia_productos():
+def referencia_productos(categoria):
     datos = cargar_datos(RUTA_JSON)
 
     referencias = []
 
-    for i in datos["productos"][0]["telefonos"]:
+    for i in datos["productos"][0][categoria]:
         referencias.append(str(i["referencia"]))
+    
+    return referencias
+
+def tipo_productos():
+    datos = cargar_datos(RUTA_JSON)
+
+    referencias = []
+
+    for i in datos["productos"][0]:
+
+        referencias.append(str(i))
     
     return referencias
