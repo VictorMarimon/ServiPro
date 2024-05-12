393db569d87664798aa1d5e0a744981ebceb0641
Victor Andres Marimon Mendoza <vmarimon1@udi.edu.co>
2024-05-12 01:29:11 -0500

    feat se agregan funciones de registros en modulo ventas

--- modulo_ventas/servicios_productos.py
+++ modulo_ventas/servicios_productos.py
@@ -2,4 +2,45 @@ import sys
 
 sys.path.append("..")
 
+from modulo_servicios.productos.consultas_productos import productos_catalogo
+from modulo_servicios.servicios.consultas_servicios import servicios_catalogo
+from datosGenerales.datos import *
 
+RUTA_JSON = "C:/Users/PC/Desktop/Proyecto---Campus/modulo_ventas/registro.json"
+
+def actualizar_registro():
+    datos = cargar_datos(RUTA_JSON)
+    for i in datos:
+            datos["productos"] = productos_catalogo()
+            datos["servicios"] = servicios_catalogo()
+            guardar_datos(datos, RUTA_JSON)
+
+def leer_registos():
+    datos = cargar_datos(RUTA_JSON)
+    cantidad_productos = 0
+    cantidad_servicios = 0
+    for llave,valor in datos.items():
+        if(llave == "servicios"):
+            print("CATALOGO SERVICIOS")
+            for j in valor:
+                cantidad_servicios += 1
+                print("")
+                print(f"Servicio No. {cantidad_servicios}")
+                print("")
+                print(f"Tipo: {j["articulo"]}")
+                print(f"Referencia: {j["referencia"]}")
+                print(f"Nombre: {j["nombre"]}")
+                print(f"Precio: {j["precio"]}")
+                print("")
+        elif(llave == "productos"):
+            print("CATALOGO PRODUCTOS")
+            for j in valor:
+                cantidad_productos += 1
+                print("")
+                print(f"Producto No. {cantidad_productos}")
+                print("")
+                print(f"Tipo: {j["articulo"]}")
+                print(f"Referencia: {j["referencia"]}")
+                print(f"Nombre: {j["nombre"]}")
+                print(f"Precio: {j["precio"]}")
+                print("")
