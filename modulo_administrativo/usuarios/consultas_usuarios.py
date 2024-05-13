4461e6890aa80d39fbf26d94d4811dc62bc7806b
Victor Andres Marimon Mendoza <vmarimon1@udi.edu.co>
2024-05-12 22:43:25 -0500

    refactor se modifica RUTA_JSON

--- modulo_administrativo/usuarios/consultas_usuarios.py
+++ modulo_administrativo/usuarios/consultas_usuarios.py
@@ -4,7 +4,7 @@ sys.path.append("..")
 
 from datosGenerales.datos import *
 
-RUTA_JSON = "C:/Users/PC/Desktop/Proyecto---Campus/usuarios/usuarios.json"
+RUTA_JSON = "C:/Users/PC/Desktop/Proyecto---Campus/modulo_administrativo/usuarios/usuarios.json"
 
 def fecha_afiliacion(documento):
     datos_json = cargar_datos(RUTA_JSON)
