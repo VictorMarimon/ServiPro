b9f6f469fbec4c508c4fb1cb46378ed938fb1532
Victor Andres Marimon Mendoza <vmarimon1@udi.edu.co>
2024-05-14 12:44:34 -0500

    refactor se modifican imports y uso de funciones

--- main.py
+++ main.py
@@ -1,30 +1,3 @@
-from modulo_ventas.crud_ventas import * 
-from modulo_ventas.servicios_productos import * 
-from modulo_ventas.catalogo_ventas import * 
-from modulo_servicios.servicios.consultas_servicios import *
-from modulo_servicios.productos.consultas_productos import *
-from modulo_servicios.productos.crud_productos import *
-from modulo_administrativo.usuarios.funcionalidades import *
-from modulo_administrativo.usuarios.crud_usuarios import *
 from modulo_menu.menu import *
-from modulo_administrativo.usuarios.consultas_usuarios import *
-from modulo_reportes.manejo_reportes import *
 
-#total_productos()
-#total_servicios()
-#actualizar_ventas("productos")
-#crear_productos()
-#eliminar_ventas()
-#actualizar_registro()
-#leer_registos()
-#catalogo_ventas()
-#actualizar_catalogo_ventas()
-#compras_usuarios()
-#usuarios_regulares_leales()
-#actualizar_usuarios()
-#ventas()
-#print(nombres_documentos_usuarios())
-#ofertas_productos("telefonos")
-#ofertas_servicios("telefonia")#
 menu_principal()
-#servicios_populares()
