25d3753c663e254bf3b5c63b214c5f45a0e11c0b
Victor Andres Marimon Mendoza <vmarimon1@udi.edu.co>
2024-05-14 00:03:38 -0500

    feat se agregan funciones

--- modulo_reportes/manejo_reportes.py
+++ modulo_reportes/manejo_reportes.py
@@ -2,3 +2,34 @@ import sys
 
 sys.path.append("..")
 
+from datosGenerales.datos import *
+
+RUTA_JSON_VENTAS = "C:/Users/PC/Desktop/Proyecto---Campus/modulo_ventas/ventas.json"
+
+def servicios_populares():
+    datos_ventas = cargar_datos(RUTA_JSON_VENTAS)
+
+    contador = 0
+
+    ventas_por_servicio = {}
+
+    for venta in datos_ventas["ventas"]:
+        if venta["categoria"] == "servicios":
+            servicio = venta["referencia_articulo"]
+            if servicio in ventas_por_servicio:
+                ventas_por_servicio[servicio] += venta["cantidad"]
+            else:
+                ventas_por_servicio[servicio] = venta["cantidad"]
+
+    print("TOP 5 SERVICIOS MÁS POPULARES")
+    print("")
+    for llave, valor in ventas_por_servicio.items():
+        contador += 1
+        if(contador < 6):
+            print(f"Servicio: {llave}, Cantidad Vendida: {valor}")
+        else:
+            break
+    
+        
+
+
